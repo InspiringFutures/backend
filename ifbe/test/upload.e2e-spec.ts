d749245fd6fe3829f54362db15d72a567dde7e50
Robin Message <robin@lambdacambridge.com>
2021-06-14 10:51:06 +0100

    Fix upload tests to run together

--- ifbe/test/upload.e2e-spec.ts
+++ ifbe/test/upload.e2e-spec.ts
@@ -31,8 +31,11 @@ async function runSQL(connection: Sequelize, path: string) {
 }
 
 const clientId = 1;
-
 const ALLOWED_CLIENT_TOKEN = 'CLIENT_TOKEN_1';
+const mediaClientJournalId = 'arbitrary1';
+const TEST_MEDIA = 'TEST MEDIA';
+const url = '/some/path';
+
 describe('Uploads from clients (e2e)', () => {
     let app: INestApplication;
 
@@ -59,7 +62,7 @@ describe('Uploads from clients (e2e)', () => {
     it('Returns 403 if no token provided', async () => {
         await authClientPost('journal', null)
             .send({
-                      clientJournalId: 'arbitrary1',
+                      clientJournalId: mediaClientJournalId,
                       type: 'media',
                       media: [
                           {clientMediaId: '/some/path', type: 'photo'},
@@ -72,7 +75,7 @@ describe('Uploads from clients (e2e)', () => {
     it('Returns 403 if wrong token provided', async () => {
         await authClientPost('journal', 'invalid')
             .send({
-                      clientJournalId: 'arbitrary1',
+                      clientJournalId: mediaClientJournalId,
                       type: 'media',
                       media: [
                           {clientMediaId: '/some/path', type: 'photo'},
@@ -86,7 +89,7 @@ describe('Uploads from clients (e2e)', () => {
     it('Text upload', async () => {
         await authClientPost('journal')
             .send({
-                clientJournalId: 'arbitrary1',
+                clientJournalId: 'arbitraryText',
                 type: 'text',
                 text: 'Some Journal entry'
             })
@@ -98,12 +101,9 @@ describe('Uploads from clients (e2e)', () => {
     });
 
     it('Single photo upload', async () => {
-        const TEST_MEDIA = 'TEST MEDIA';
-        const url = '/some/path';
-
         const response = await authClientPost('journal')
             .send({
-                clientJournalId: 'arbitrary1',
+                clientJournalId: mediaClientJournalId,
                 type: 'media',
                 media: [
                     {url, type: 'photo'},
@@ -132,74 +132,48 @@ describe('Uploads from clients (e2e)', () => {
     });
 
     it('Multiple media upload', async () => {
-        const TEST_MEDIA = 'TEST MEDIA';
-        const url = '/some/path';
-        const ids = [1, 2, 3];
-
-        const response = await authClientPost('journal')
-            .send({
-                clientJournalId: 'arbitrary1',
-                type: 'media',
-                media: ids.map(id => ({url: url + id, type: 'photo'})),
-                caption: 'Some caption',
-            })
-            .expect(201)
-            .expect((res) => {
-                if (res.body.clientId != 1) throw new Error("Incorrect client");
-                if (!('id' in res.body)) throw new Error("missing id");
-            });
-        const journalId = response.body.id;
-
-      const xref = 'GROUP:PARTICIPANT:' + journalId + ':' + response.body.createdAt;
-      const journalEntryIds = await Promise.map(ids, async id => ({id, journalEntryId: (await authClientPost(`journal/${journalId}/media`)
-            .field('url', url + id)
-            .field('xref', xref)
-            .attach('upload', Buffer.from(TEST_MEDIA + id), 'ignore the filename')
-            .expect(201)).body.id}));
+        const {journalEntryIds, journalId} = await uploadMedia();
 
         const journalService = app.get(JournalService);
         await Promise.all(journalEntryIds.map(async ({id, journalEntryId}) => {
             const mediaUrl = await journalService.getMediaUrl(1, journalId, journalEntryId);
           const response = await httpGet(mediaUrl);
           expect(response.body).toEqual(TEST_MEDIA + id);
-          expect(response.headers['x-amz-meta-xref']).toEqual(xref);
+          expect(response.headers['x-amz-meta-xref']).toEqual('xref');
         }));
     });
 
-    const TEST_MEDIA = 'TEST MEDIA';
-    const url = '/some/path';
-
     async function uploadMedia() {
-
         const initialIds = [1, 2, 3];
         const initialMedia = initialIds.map(id => ({ url: url + id, type: 'photo' }));
 
         const initialResponse = await authClientPost('journal')
             .send({
-                clientJournalId: 'arbitrary1',
+                clientJournalId: mediaClientJournalId,
                 type: 'media',
                 media: initialMedia,
                 caption: 'Some caption',
             });
         const journalId = initialResponse.body.id;
-        await Promise.map(initialIds, async id => ({
+
+        const journalEntryIds = await Promise.map(initialIds, async id => ({
             id, journalEntryId: (await authClientPost(`journal/${journalId}/media`)
                 .field('url', url + id)
                 .field('xref', 'xref')
                 .attach('upload', Buffer.from(TEST_MEDIA + id), 'ignore the filename')
                 .expect(201)).body.id,
         }));
-        return journalId;
+        return {journalId, journalEntryIds};
     }
 
     it('Change media upload', async () => {
-        const journalId = await uploadMedia();
+        const {journalId} = await uploadMedia();
 
         const updatedIds = [2, 3, 4];
         const updatedMedia = updatedIds.map(id => ({url: url + id, type: 'photo'}));
         const updatedResponse = await authClientPost('journal')
             .send({
-                clientJournalId: 'arbitrary1',
+                clientJournalId: mediaClientJournalId,
                 type: 'media',
                 media: updatedMedia,
                 caption: 'New caption',
@@ -232,12 +206,13 @@ describe('Uploads from clients (e2e)', () => {
 
         const existingContents = await storageService._test_listing();
 
-        const journalId = await uploadMedia();
+        await uploadMedia();
+
         await request(app.getHttpServer())
             .delete(`/api/client/${clientId}/journal`)
             .set('X-Token', ALLOWED_CLIENT_TOKEN)
             .send({
-                clientJournalId: 'arbitrary1',
+                clientJournalId: mediaClientJournalId,
             })
             .expect(200);
 
