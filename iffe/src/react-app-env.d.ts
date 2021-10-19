b36d392e7cbbaeb421e48d153cd529e91d670a9a
Robin Message <robin@lambdacambridge.com>
2021-10-19 14:38:47 +0100

    Survey editor: trying an audio library, but only produces webm+opus which isn't iOS compatible

--- iffe/src/react-app-env.d.ts
+++ iffe/src/react-app-env.d.ts
@@ -1,1 +1,20 @@
 /// <reference types="react-scripts" />
+
+declare module 'audio-react-recorder' {
+    enum RecordState {
+        START = 'start',
+        PAUSE = 'pause',
+        STOP = 'stop',
+        NONE = 'none',
+    }
+    interface AudioReactRecorderProps {
+        state: RecordState;
+        onStop: (data) => void;
+    }
+
+    class AudioReactRecorder extends React.Component<AudioReactRecorderProps, any> {} {
+
+    }
+
+    export default AudioReactRecorder
+}
--- iffe/src/survey/AudioDialog.tsx
+++ iffe/src/survey/AudioDialog.tsx
@@ -8,6 +8,10 @@ import {
 } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { useReactMediaRecorder } from "react-media-recorder";
+
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import StopIcon from "@material-ui/icons/Stop";
 
 import { Spacer } from "./Spacer";
 import { TextWithOptionalAudio } from "./SurveyContent";
@@ -20,6 +24,38 @@ export interface AudioDialogRef {
     open: (current: TextWithOptionalAudio, onSave: SaveFunc) => void;
 }
 
+function withoutAudio(current?: TextWithOptionalAudio) {
+    if (current === undefined) {
+        return undefined;
+    }
+    if (typeof current === 'string') {
+        return current;
+    }
+    return {
+        text: current.text,
+    };
+}
+
+function withAudio(current: TextWithOptionalAudio, currentRecording: any) {
+    return {
+        text: extractText(current),
+        audio: currentRecording,
+    };
+}
+
+function extractAudio(current: TextWithOptionalAudio) {
+    return typeof current === 'string' ? undefined : current.audio;
+}
+
+const convertToBase64 = (blob: Blob): Promise<string | undefined> => {
+    return new Promise((resolve, reject)=> {
+        const reader = new FileReader();
+        reader.readAsDataURL(blob);
+        reader.onload = () => resolve(reader.result?.toString());
+        reader.onerror = error => reject(error);
+    });
+}
+
 export const AudioDialog = forwardRef<AudioDialogRef>((props, ref) => {
     const classes = useStyles();
 
@@ -29,38 +65,81 @@ export const AudioDialog = forwardRef<AudioDialogRef>((props, ref) => {
     const [current, setCurrent] = useState<TextWithOptionalAudio>();
     const [onSave, setOnSave] = useState<SaveFunc>();
 
+    const [currentRecording, setCurrentRecording] = useState<string>();
+
+    const {
+        status,
+        startRecording,
+        stopRecording,
+        mediaBlobUrl,
+    } = useReactMediaRecorder({
+        audio: true,
+        video: false,
+        blobPropertyBag: {type: 'audio/mp4'},
+        onStop,
+    });
+
     useImperativeHandle(ref, () => ({
         open: (current, onSave) => {
             setOpen(true);
             setCurrent(current);
             setOnSave(onSave);
+            setCurrentRecording(extractAudio(current));
         },
     }), [setOpen]);
+
+    function deleteAudio() {
+        onSave?.(withoutAudio(current));
+        close();
+    }
+
+    function saveAudio() {
+        current && onSave?.(withAudio(current, currentRecording));
+        close();
+    }
+
+    async function onStop(blobUrl: string, blob: Blob) {
+        const audio = await convertToBase64(blob);
+        const header = (await blob.text()).substring(0, 200);
+        console.log("Recording", blobUrl, blob, header);
+        setCurrentRecording(audio);
+    }
 
     return (
         <Dialog
             open={open}
             onClose={close}
-            scroll="paper"
+            scroll="body"
             fullWidth
         >
             <DialogTitle disableTypography className={classes.previewTitle}>
-                <Typography variant="h6">Audio recording</Typography>
-                <Spacer/>
+                <p>{extractText(current)}</p>
             </DialogTitle>
             <DialogContent dividers>
-                Current text: {extractText(current)}
+                {status === "recording" && <div>
+                    Recording...
+                    <Button startIcon={<StopIcon />} onClick={stopRecording}>
+                        Stop
+                    </Button>
+                </div>}
+                {status !== "recording" && <div>
+                    <Button startIcon={<FiberManualRecordIcon />} onClick={startRecording}>
+                        Record
+                    </Button>
+                    <br />
+                    {mediaBlobUrl && <audio src={mediaBlobUrl} controls />}
+                </div>}
             </DialogContent>
             <DialogActions>
-                <Button onClick={close} color="secondary">
+                {current && extractAudio(current) && <Button onClick={deleteAudio} color="secondary">
                     Delete
-                </Button>
+                </Button>}
                 <Button onClick={close}>
                     Close
                 </Button>
-                <Button onClick={close} color="primary">
+                {currentRecording && <Button onClick={saveAudio} color="primary">
                     Save
-                </Button>
+                </Button>}
             </DialogActions>
         </Dialog>
     );
